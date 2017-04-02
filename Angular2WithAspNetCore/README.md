69ab7716bcf02c11f4cce379dc1ca5c7d4710bd0
Fatih Türker <fatihturker35@gmail.com>
2017-04-02 23:41:34 +0300

    solution edit

---
+++ Angular2WithAspNetCore/README.md
@@ -0,0 +1,2 @@
+# Angular 2 with Asp.Net Core 
+[![Build Status](https://travis-ci.org/fatihturker/Angular2WithAspNetCore.svg?branch=master)](https://travis-ci.org/fatihturker/Angular2WithAspNetCore)
---
+++ Angular2WithAspNetCore/wwwroot/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-component',
+    template: `<h1>My First {{appName}}</h1>`,
+})
+export class AppComponent {
+    appName = 'Angular 2 App with ASP.NET Core';
+}
